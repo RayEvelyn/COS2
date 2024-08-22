b84e3ac50685dbaffd4eab40a0a071cef6c72ce2
Greyson Lentz <GBLentz@student.fullsail.edu>
2024-08-22 09:44:53 -0600

    I think i'm getting there. Still don't know if i'll have anything to show for build review.

--- vector/src/main/java/im/vector/app/features/settings/VectorSettingsCalendar.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorSettingsCalendar.kt
@@ -1,4 +1,15 @@
 package im.vector.app.features.settings
 
-class VectorSettingsCalendar {
+import dagger.hilt.android.AndroidEntryPoint
+import im.vector.app.R
+
+@AndroidEntryPoint
+abstract class VectorSettingsCalendar :
+    VectorSettingsBaseFragment() {
+
+    override var titleRes = R.string.calendar
+    override val preferenceXmlRes = R.layout.layout_calendar
+
+
+
 }
